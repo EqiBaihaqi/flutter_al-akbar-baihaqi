372d44e553fb0116dcd4dfe7d8aa728a5dacb891
EqiBaihaqi <baihaqi08.aa@gmail.com>
2024-03-05 23:38:16 +0700

    Add soal prioritas 1

---
+++ 6_DartObject Oriented Programming/praktikum/soal_Prioritas1.dart
@@ -0,0 +1,41 @@
+void main(List<String> args) {
+  var m1 = Mobil();
+  m1.kapasitas = 50;
+  m1.muatan = [10, 20, 10];
+  m1.Berat = 10;
+  print(m1.tambahMuatan());
+}
+
+//Membuat sebuah class Hewan yang memiliki property Berat
+class Hewan {
+  int? Berat;
+
+  Hewan() {
+    Berat = 0;
+  }
+}
+
+//Membuat class Mobil yang berisi property dan method dan extend class Hewan
+class Mobil extends Hewan {
+  int? kapasitas;
+  List? muatan;
+//constuctor dari Mobil
+  Mobil() {
+    kapasitas = 0;
+    muatan = [];
+  }
+
+  tambahMuatan() {
+    //Membuat kondisi diamana jika kapasitas lebih dari total muatan, maka
+    //data Hewan akan ditambahklan ke dalam list muatan
+    if (kapasitas! >
+        muatan!.fold(0, (previousValue, element) => previousValue + element)) {
+      muatan?.add(Berat);
+    //Melakuakn return pada list muatan yang telah ditambahkan data baru
+      return muatan;
+    //Melakukan return muatan tanpa adanya penambahan data baru  
+    } else {
+      return muatan;
+    }
+  }
+}
