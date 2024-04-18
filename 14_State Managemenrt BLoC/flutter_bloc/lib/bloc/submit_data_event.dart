cd078cf55917e0e0a396cbae681a38fc91408880
EqiBaihaqi <baihaqi08.aa@gmail.com>
2024-04-18 23:58:34 +0700

    Copy previous project

--- 14_State Managemenrt BLoC/flutter_bloc/lib/bloc/submit_data_event.dart
+++
@@ -1,4 +0,0 @@
-part of 'submit_data_bloc.dart';
-
-@immutable
-sealed class SubmitDataEvent {}
--- 14_State Managemenrt BLoC/flutter_bloc/lib/bloc/submit_data_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'submit_data_bloc.dart';
-
-@immutable
-sealed class SubmitDataState {}
-
-final class SubmitDataInitial extends SubmitDataState {}
